bb9e656abc62588badadf1b6148ffda42b017b68
kouta <kouta@Afra>
2023-10-16 13:05:15 +0100

    My 1st commit

--- src/TP1/Client.java
+++ src/TP1/Client.java
@@ -1,3 +1,4 @@
+package TP1;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
--- src/TP1/Server.java
+++ src/TP1/Server.java
@@ -1,3 +1,4 @@
+package TP1;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
